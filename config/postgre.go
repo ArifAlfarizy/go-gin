fd5df02801a77126011379ecf66f03339749853f
Arif Alfarizy <arifalfrzy@gmail.com>
2026-05-27 10:00:17 +0700

    fix: minor fix

--- config/postgre.go
+++ config/postgre.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -15,9 +17,21 @@ type Todo struct { // JSON tag				// GORM tag
 
 var DB *gorm.DB
 
+
 func ConnectDB() {
 	var err error
-	dsn := "host=localhost user=alfarizy dbname=gin_test port=5432 sslmode=disable"
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s dbname=%s port=%s sslmode=disable",
+		host,
+		user,
+		dbname,
+		port,
+	)
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
